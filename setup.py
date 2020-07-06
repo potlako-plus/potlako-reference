01d22e3d5fa246d5185413c09d512b841de8e909
tumiemosweu <imosweuk@gmail.com>
2020-07-06 11:11:37 +0200

    update reference models

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
-
-from setuptools import find_packages
 from setuptools import setup
-
+from setuptools import find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -13,17 +11,17 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='potlako-reference',
-    version='0.1.0',
-    author=u'BHP Software Engineering & Data Management Center',
+    version='0.0',
+    author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/potlako-plus/potlako-reference',
     license='GPL license, see LICENSE',
-    description='potlako edc metadata-rules',
+    description='potlako plus reference',
     long_description=README,
     zip_safe=False,
-    keywords='django cancer',
+    keywords='potlako plus reference',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
